2e7094c574db1d5289c18e949053ee0e4824ac7d
Dan O'Huiginn <daniel@ohuiginn.net>
2015-02-06 20:50:55 +0100

    working on sic slurping

--- companies_by_sic.py
+++ companies_by_sic.py
@@ -1,5 +1,7 @@
 import requests
 from lxml import html
+from mrjob.job import MRJob
+from mrjob.protocol import JSONValueProtocol
 
 def by_sic_corpwatch(sic):
     # does not work
@@ -11,14 +13,38 @@ def by_sic_corpwatch(sic):
     r = requests.get(url, params=params)
     return r.json()
 
-def by_sic(sic):
-    url = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=%s&owner=include&match=&start=0&count=4999&hidefilings=0' % sic
-    r = requests.get(url)
-    doc = html(r.text)
-    rows = doc.findall('.//tr')
-    companies = []
-    for row in rows[1:]: # ditch the headers
-        cik, name, state = [x.text_content() for x in row.findall('.//td')]
-        cik = cik.lstrip('0') # official CIKs have leading zeroes, but they are painful for us
-        yield [cik, name, state, sic]
+class EdgarIndices(MRJob):
+
+    def mapper(self, _, x):
+        for year in range(1993, 2016):
+            for qtr in [1,2,3,4]:
+                urlpattern = 'ftp://ftp.sec.gov/edgar/full-index/2014/QTR%s/company.gz' % (year, qtr)
+                yield urlpattern
+
+class SICCorps(MRJob):
+    '''
+    input: SIC codes, one per line
+    output: tab-delimited cik, name, company, sic for companies with that SIC
+    '''
+
+    OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def mapper(self, _, sic):
+        url = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=%s&owner=include&match=&start=0&count=4999&hidefilings=0' % sic
+        r = requests.get(url)
+        doc = html.fromstring(r.text)
+        rows = doc.findall('.//tr')
+        companies = []
+        for row in rows[1:]: # ditch the headers
+            cik, name, state = [x.text_content() for x in row.findall('.//td')]
+            cik = cik.lstrip('0') # official CIKs have leading zeroes, but they are painful for us
+            yield None, {
+                'cik': cik,
+                'name': name,
+                'state': state,
+                'sic': sic}
+
+
+if __name__ == '__main__':
+    SICCorps.run()
 
