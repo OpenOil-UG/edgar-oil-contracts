ccf52e6c0017dc126a014b5afa835794da9f6673
Dan O'Huiginn <daniel@ohuiginn.net>
2015-03-02 15:34:29 +0100

    push namesearch results back into the spreadsheet

--- dissect/sheetnamesearch.py
+++ dissect/sheetnamesearch.py
@@ -6,6 +6,10 @@ Prepare names for working on the searches
 
 import re
 import tempfile
+import json
+import codecs
+
+from collections import defaultdict
 
 from dissect import sheets
 
@@ -50,6 +54,7 @@ def names_searchable(rowcount = None):
 
 def searchterm_file():
     '''
+    generate file of search terms, one regex per line. return the filename
     assumes names_searchable has already been run
     '''
     sheet = get_search_sheet()
@@ -59,6 +64,49 @@ def searchterm_file():
         lines = [x.encode('utf-8') for x in lines]
         tfile.writelines(lines)
     return tfile.name
+
+
+
+def reconcile_matches(fn, clear = True):
+    '''
+    '''
+    sheet = get_search_sheet()
+
+    terms = sheet.range('B2:B%s' % sheet.row_count)
+    line_numbers = dict(zip([x.value for x in terms], range(2,sheet.row_count+1)))
+
+    match_counts = sheet.range('D2:D%s' % sheet.row_count)
+    output_values = sheet.range('E2:E%s' % sheet.row_count)
+
+    if clear:
+        for ov in output_values:
+            ov.value = ''
+
+    matches = defaultdict(list)
+    for line in codecs.open(fn):
+        js = json.loads(line)
+        url = js['filepath'].replace('/data/sedar', 'https://sedar.openoil.net.s3.amazonaws.com')
+        for positive, details in js['positives'].items():
+            positive = re.escape(positive) # re-escape
+            lineno = line_numbers.get(positive, None)
+            if lineno is None:
+                print('no lineno for %s' % positive)
+                continue
+
+            matches[lineno].append(url)
+    output = [', '.join(matches[lineno]) for lineno in range(2,sheet.row_count+1)]
+    for lineno in range(2, sheet.row_count+1):
+        if matches[lineno] or clear:
+            if False and lineno > len(output_values):
+                print('skipping silly line number')
+                continue
+            output_values[lineno-2].value = ', '.join(matches[lineno])
+            match_counts[lineno-2].value = len(matches[lineno])
+    sheet.update_cells(output_values)
+    sheet.update_cells(match_counts)
+    print('done')
+            
+
 
 
 if __name__ == '__main__':
