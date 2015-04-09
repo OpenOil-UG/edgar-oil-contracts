2b26dc85e463cc470e3ed04b5bb9d293ab108c61
Dan O'Huiginn <daniel@ohuiginn.net>
2015-04-09 10:51:39 +0200

    tweak to filepath conversion

--- dissect/util/__init__.py
+++ dissect/util/__init__.py
@@ -3,4 +3,5 @@ def filename_to_s3path(fn):
     fn = fn.replace('/data/sedar/mining/', 'https://sedar.openoil.net.s3.amazonaws.com/')
     fn = fn.replace('/data/sedar/', 'https://sedar.openoil.net.s3.amazonaws.com/')
     fn = fn.replace('/data/oil', 'https://sec-edgar.openoil.net.s3.amazonaws.com/oil')
+    fn = fn.replace('s3://sec-edgar.openoil.net', 'https://sec-edgar.openoil.net.s3.amazonaws.com')
     return fn
